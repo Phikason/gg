cf8f6a9863f9c556384d7514f255f9fa7b608c7f
Chanapa Phikhason <150318273+Phikason@users.noreply.github.com>
2024-03-07 00:52:26 +0700

    first commit

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import styles from './Portfolio.module.css'
 
 const user={
-  img1:'https://scontent.xx.fbcdn.net/v/t1.15752-9/431037272_1733450340511528_2291746480481955818_n.jpg?stp=dst-jpg_s206x206&_nc_cat=105&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeFeE4x-o_egmXbo4VnBc0LECbTpaSWr_EYJtOlpJav8RuxUBHxUN3p9MhcrKXDKJGk0pOFFtsVSwUEId9cEbU67&_nc_ohc=z3UCFuxIivIAX-DO1Ak&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_AdRSjiZYw5f3_tm5_0AJHTsi18FMODHt-KMDQkf4xN7ywg&oe=6610263B',
-  img2:'https://scontent.xx.fbcdn.net/v/t1.15752-9/430906159_7164471036999204_6681903036145743427_n.jpg?stp=dst-jpg_s206x206&_nc_cat=109&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeEYT5HFLGf2oGADtrYsAcl9uQL_0BG9_V65Av_QEb39Xi5zhU_HHGts66F3phpW8RjoP9ddKmUqv_Uztd_Py7r-&_nc_ohc=hJPCsfXmTNUAX_HPEUW&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_AdSf0S3_RBrs2NAEgfTKdU6l5Q9b_6RZlRIu6vcVfsUaBw&oe=66100572',
-  img3:'https://scontent.xx.fbcdn.net/v/t1.15752-9/431064626_1422295165041462_4350541804144245755_n.jpg?stp=dst-jpg_s280x280&_nc_cat=103&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeGrVqjIkcjk5bfKQF3oMeIkwR-ipE-V6YfBH6KkT5Xph6w2QsJVvtPREWH8G4wPONV5nev5ssevEeG9Eb4r3E47&_nc_ohc=jIKWjfZXtBYAX-acZsv&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_AdSWRirj7nY6NTcxNo3SBMqogcCN6-EWil1WqJvT32ZEsQ&oe=6610198D,
-  img4:'https://scontent.xx.fbcdn.net/v/t1.15752-9/423542016_368064682710896_6048108897471305033_n.jpg?stp=dst-jpg_s240x240&_nc_cat=101&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeGGGlHBt9_KuMqJWGqUCSR1VoSYb0iRA2dWhJhvSJEDZ38Qgrnc5Cu8X450orRN_cRPB2gADXkis-USl7LGOp_p&_nc_ohc=o2wKseNtgVMAX-cCiF1&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_AdT42CAmf2-G4X6Xp7chh4FJAwApPck5eTu_sYg3lCuN2g&oe=660FF7ED',
-  img5:'https://scontent.xx.fbcdn.net/v/t1.15752-9/423472521_817810943514689_7501626910692316448_n.jpg?stp=dst-jpg_s206x206&_nc_cat=108&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeEjibxGOR6e66k5teOo4zxkBJhwV5MZueEEmHBXkxm54RKx35PEh0bgyF5xpxMCAKyqw1q41UJcTD6xnqCpbLXO&_nc_ohc=1uaiHkMlrXYAX92z7xA&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_AdSy43KT4w34ZsvIZPQiOlTAV-P_Fw2TchsLLH8jVoSP-w&oe=660FFA72',
-  img6:'https://scontent.xx.fbcdn.net/v/t1.15752-9/423472232_935555434954622_4354488470821145354_n.jpg?stp=dst-jpg_s206x206&_nc_cat=109&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeHsr2ILk_DFJ8TFCudSFYgjWGjTapWEY2NYaNNqlYRjY1KJb-uMifrIGC2PrZwCzqxwK2ZnKaG2TZsfL23GTApR&_nc_ohc=KWyWuAqeNKUAX9pj2_B&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_AdTr8_J_MDMkGuBRG6X2fELfrX1gaZFmyfV7HP5pgR8gjg&oe=6610184B',
+  img1:'https://i.pinimg.com/564x/97/ef/24/97ef241b207714a81b26d15d7371dd10.jpg',
+  img2:'https://i.pinimg.com/564x/14/d5/ca/14d5ca674ec4edbe085b4076998a74b8.jpg',
+  img3:'https://i.pinimg.com/564x/81/35/68/813568309fe66142dfa80e8abeb1b98d.jpg',
+  img4:'https://i.pinimg.com/564x/12/9e/95/129e95ea0d81790b1a154bc6fbe6ca7e.jpg',
+  img5:'https://i.pinimg.com/564x/7a/31/f6/7a31f6ac35cc24363f2f8983154689b9.jpg',
+  img6:'https://i.pinimg.com/564x/9e/ef/e0/9eefe0e34f8f3e5a781ecb9d5ee0db56.jpg',
 }
 function Portfolio() {
   return (
--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import styles from "./Testimonials.module.css";
 
 const user={
-  img1:'https://scontent.xx.fbcdn.net/v/t1.15752-9/423599700_789022719740462_9167719433049016428_n.jpg?stp=dst-jpg_p206x206&_nc_cat=101&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeHAEvXEkH9jc0_i9PG0YMloyccMp-0lOeHJxwyn7SU54WyiVdDR5jNNk7Lnz16iDa2NpUf0qYFe2eGxQwCCZbeW&_nc_ohc=WhdDHILd9NYAX964cwo&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_AdSnzwQ5Q9b71ZCzAXvAlh9bj1NttxV9DeUOYs7gt_kNEw&oe=66101C8E',
-  img2:'https://scontent.xx.fbcdn.net/v/t1.15752-9/430862840_1344631126932853_3112375142991197116_n.jpg?stp=dst-jpg_p206x206&_nc_cat=105&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeGS1F09nLMNTDyqEr7yL1epEozu5pmQKJ4SjO7mmZAonv5FeD_Adh7TTwv9pM_fitlTNPVo7ts6f0pJpeWXEpvO&_nc_ohc=iKDGqY-Tr5MAX-mlhcC&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_AdSRcqiP9tPq4i8rltlo7HWPtMTxwAGxOtTR0jTYAcXN5A&oe=66101C89',
-  img3:'https://scontent.xx.fbcdn.net/v/t1.15752-9/423599675_1113314856755423_3519051644682838585_n.jpg?stp=dst-jpg_p206x206&_nc_cat=102&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeFXIKj_jFDoZY1JmDudk43np8K-Ior8Eq6nwr4iivwSrt8At92UyoOjkKkWc3cBBbmhfHg2xtmPGxyNAJaCqLDr&_nc_ohc=AuTsIyrKQaoAX-LqE4c&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=03_AdSWMMqNpYL6e6ngjY3wmhQpRzs7PLkfbg1wwXsgdWj1aQ&oe=661027AC',
+  img1:'https://i.pinimg.com/236x/0d/17/42/0d1742c00146045a28426c10ed9009d4.jpg',
+  img2:'https://i.pinimg.com/474x/5a/16/5b/5a165be073dd1290572bc1cdf84da3d1.jpg',
+  img3:'https://i.pinimg.com/564x/a0/04/fa/a004fade84c27ed3965da551f92aae34.jpg',
 }
 function Testimonials() {
   return (
@@ -19,7 +19,7 @@ function Testimonials() {
             nesciunt!
           </p>
           <img
-            src="S__17825811_0.jpg"
+            src={user.img1}
             alt=""
           />
           <h4>Mango</h4>
@@ -33,7 +33,7 @@ function Testimonials() {
             nesciunt!
           </p>
           <img
-            src="S__17825814_0.jpg"
+            src={user.img2}
             alt=""
           />
           <h4>Orange</h4>
@@ -47,7 +47,7 @@ function Testimonials() {
             nesciunt!
           </p>
           <img
-            src="S__17825813_0.jpg"
+            src={user.img3}
             alt=""
           />
           <h4>Watermelon</h4>
